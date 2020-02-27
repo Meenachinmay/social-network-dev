2705993470353353097c6f51081391d0d7e75363
meenachinmay <chinmayanand896@gmail.com>
2020-02-27 14:13:41 +0530

    mongodb is connected

--- server.js
+++ server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 
 const app = express();
 
-app.get('/', (req, res) => res.send('hello'));
+// DB config
+const db = require('./config/keys').mongoURI;
+
+// Connect to database
+mongoose.connect(db)
+    .then(() => console.log('Mongo DB Connected', 
+        { useNewUrlParser: true },
+        { useCreateIndex: true},
+        { useUnifiedTopology: true }
+    ))
+    .catch(err => console.log(err));
+
+
+app.get('/', (req, res) => res.send('hello world'));
 
 const port = process.env.PORT || 5000;
 
