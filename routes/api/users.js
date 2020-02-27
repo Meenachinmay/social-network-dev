6fc1fd3fd9bf33a474d2fb8d8bc2b7527ead9d1f
meenachinmay <chinmayanand896@gmail.com>
2020-02-27 15:24:54 +0530

    user login added without validation

--- routes/api/users.js
+++ routes/api/users.js
@@ -51,4 +51,34 @@ router.post('/register', (req, res) => {
         });
 });
 
+
+// @route GET api/users/login
+// @desc  Login a user
+// @access Public
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // find by email
+    User.findOne({email})
+        .then(user => {
+            // check for user
+            if (!user){
+                return res.status(404).json({
+                    email: 'User email not found.'
+                })
+            }
+
+            // check password
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if (isMatch){
+                        return res.json({msg: 'Success'})
+                    }else{
+                        return res.status(400).json({password: 'Password is not matched to our database.'})
+                    }
+                })
+        });
+})
+
 module.exports = router;
