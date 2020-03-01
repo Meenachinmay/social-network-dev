c779cccb8cb3b0e31b81e91ef1e1895280063f81
meenachinmay <chinmayanand896@gmail.com>
2020-03-01 16:49:18 +0530

    post like and unlike route added

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -71,4 +71,48 @@ router.delete('/:id', passport.authenticate('jwt', {session: false}), (req,res) 
         }).catch(err => res.status(401).json({ noprofileFound: 'No profile found'}));
 });
 
+
+// @route POST api/posts/like/:id
+// @desc  Like posts
+// @access Private
+router.post('/like/:id', passport.authenticate('jwt', {session: false}), (req,res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length > 0){
+                        return res.status(400).json({ alreadyLiked: 'User liked this post'});
+                    }
+
+                    post.likes.unshift({ user: req.user.id });
+
+                    post.save().then(post => res.json({msg: 'Liked', post: post})).catch(err => res.json(err));
+
+                }).catch(err => res.status(404).json({ postnotfound: 'Post not found'}));
+        }).catch(err => res.status(401).json({ noprofileFound: 'No profile found'}));
+});
+
+
+// @route POST api/posts/unlike/:id
+// @desc  Dislike posts
+// @access Private
+router.post('/unlike/:id', passport.authenticate('jwt', {session: false}), (req,res) => {
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            Post.findById(req.params.id)
+                .then(post => {
+                    if (post.likes.filter(like => like.user.toString() === req.user.id).length === 0){
+                        return res.status(400).json({ notLiked: 'You have not yet liked this post'});
+                    }
+
+                    removeIndex = post.likes.map(item => item.user.toString()).indexOf(req.user.id);
+
+                    post.likes.splice(removeIndex, 1);
+
+                    post.save().then(post => res.json({msg: 'Unliked', post: post})).catch(err => res.json(err));
+
+                }).catch(err => res.status(404).json({ postnotfound: 'Post not found'}));
+        }).catch(err => res.status(401).json({ noprofileFound: 'No profile found'}));
+});
+
 module.exports = router;
