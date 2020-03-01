10b1d739a9f9c419de007839ff255ebaf0086eea
meenachinmay <chinmayanand896@gmail.com>
2020-03-02 01:15:04 +0530

    navbar, landing and footer added into reactapp

---
+++ client/src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+    return (
+        <footer class="bg-dark text-white mt-5 p-4 text-center">
+            Copyright &copy; 2020 Social-Network-Dev
+        </footer>
+    )
+}
