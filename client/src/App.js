4886d21bdfe490d751d75d44ebcf37df9323b7a0
meenachinmay <chinmayanand896@gmail.com>
2020-03-06 22:36:47 +0530

    react router dom added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
 
 import Navbar from './components/layout/Navbar';
 import Footer from './components/layout/Footer';
 import Landing from './components/layout/Landing';
 
+import Login from './components/auth/login';
+import Register from './components/auth/register';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <Landing />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  render(){
+    return (
+      <Router>
+        <div className="App">
+          <Navbar />
+            <Route exact path="/" component={ Landing }/>
+            <Route path="/login" component={ Login }/>
+            <Route path="/register" component={ Register }/>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
