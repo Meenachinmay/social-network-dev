7e4fce168e2249282678c17d96c9c0f368347233
meenachinmay <chinmayanand896@gmail.com>
2020-03-07 00:36:23 +0530

    login fields are working with react

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react';
 
 class Login extends Component{ 
+
+    constructor(){
+        super();
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onChange(e){
+        this.setState({[ e.target.name ]: e.target.value })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+
+        const loggedInUser = {
+            email: this.state.email,
+            password: this.state.password
+        }
+
+        console.log(loggedInUser);
+    }
+
     render(){
         return(
         <div className="login">
@@ -9,12 +37,12 @@ class Login extends Component{
                 <div className="col-md-8 m-auto">
                 <h1 className="display-4 text-center">ログイン</h1>
                 <p className="lead text-center">あなたのアカウントでログインしてください。</p>
-                <form action="dashboard.html">
+                <form onSubmit={ this.onSubmit }>
                     <div className="form-group">
-                    <input type="email" className="form-control form-control-lg" placeholder="メールアドレス" name="email" />
+                    <input type="email" className="form-control form-control-lg" placeholder="メールアドレス" name="email" value={ this.state.email } onChange={ this.onChange }/>
                     </div>
                     <div className="form-group">
-                    <input type="password" className="form-control form-control-lg" placeholder="パスウード" name="password" />
+                    <input type="password" className="form-control form-control-lg" placeholder="パスウード" name="password" value={ this.state.password } onChange={ this.onChange }/>
                     </div>
                     <input type="submit" className="btn btn-info btn-block mt-4" />
                 </form>
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom';
 export default class Landing extends Component {
     render() {
         return (
-            <div class="landing">
-                <div class="dark-overlay landing-inner text-light">
-                <div class="container">
-                    <div class="row">
-                    <div class="col-md-12 text-center">
-                        <h1 class="display-3 mb-4">ソーシャルネットワーク
+            <div className="landing">
+                <div className="dark-overlay landing-inner text-light">
+                <div className="container">
+                    <div className="row">
+                    <div className="col-md-12 text-center">
+                        <h1 className="display-3 mb-4">ソーシャルネットワーク
                         </h1>
-                        <p class="lead">新しい開発者プロファイルを作って、共有と投稿して, 他の開発者から助けを得るための</p>
+                        <p className="lead">新しい開発者プロファイルを作って、共有と投稿して, 他の開発者から助けを得るための</p>
                         <hr />
-                        <Link to="register" class="btn btn-lg btn-info mr-2">サインアップ</Link>
-                        <Link to="login" class="btn btn-lg btn-light">ログイン</Link>
+                        <Link to="register" className="btn btn-lg btn-info mr-2">サインアップ</Link>
+                        <Link to="login" className="btn btn-lg btn-light">ログイン</Link>
                     </div>
                     </div>
                 </div>
