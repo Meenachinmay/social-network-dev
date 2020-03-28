73e2384c1837d0dbd8473aa03c176a454af3678a
meenachinmay <chinmayanand896@gmail.com>
2020-03-28 23:42:20 +0530

    token is now persisting after login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+import jwt_decode from 'jwt-decode';
+import setAuthToken from './utils/setAuthToken';
+import { setCurrentUser } from './actions/authActions';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -12,6 +15,16 @@ import Login from './components/auth/login';
 import Register from './components/auth/register';
 
 import './App.css';
+
+// Check for token
+if (localStorage.jwtToken){
+  // set auth token header auth
+  setAuthToken(localStorage.jwtToken);
+  // decode taken and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+  // set user and isAuthtenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends React.Component {
   render(){
